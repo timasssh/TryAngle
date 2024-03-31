11a75538faf4c8195375f7426a004e7b95a94421
timasssh <timmylbueno@gmail.com>
2024-03-31 20:35:45 -0300

    basic setup and creation of all the files and directories of the project

---
+++ source/assets/scripts/index-script.js
@@ -0,0 +1,4 @@
+import Triangle from "../../models/Triangle.js";
+
+let triangle = new Triangle(6, 8, 10);
+console.log(triangle)
---
+++ source/models/Triangle.js
@@ -0,0 +1,7 @@
+export default class Triangle {
+    constructor (firstSide, secondSide, thirdSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+    }
+}
