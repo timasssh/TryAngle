01793ce08af26d9674bf6f40bbdaabe4f604c4f5
timasssh <timmylbueno@gmail.com>
2024-03-31 23:00:11 -0300

    checkTriangle method fixed, now: if the triangle is valid returns true and in isn't valid returns false

--- source/models/Triangle.js
+++ source/models/Triangle.js
@@ -1,11 +1,10 @@
 export default class Triangle {
     constructor (firstSide, secondSide, thirdSide) {
-        this.firstSide = firstSide;
-        this.secondSide = secondSide;
-        this.thirdSide = thirdSide;
+        this.firstSide = Number(firstSide);
+        this.secondSide = Number(secondSide);
+        this.thirdSide = Number(thirdSide);
     }
     checkTriangle() {
-        // FIXME
         return ((this.firstSide + this.secondSide) > this.thirdSide && (this.secondSide + this.thirdSide) > this.firstSide && (this.firstSide + this.thirdSide) > this.secondSide);
     }
 }
