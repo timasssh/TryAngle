6678abf2b89a92c07a82c7704debb320e7a6d265
timasssh <timmylbueno@gmail.com>
2024-04-01 05:30:39 -0300

    creation of an function in the index-script to write the result of the Triangle class method; fixed bug on the inputs type using parseFloat to ignore strings and use just numbers; creation of a p tag to show the result of the index-script function and adjustments on the form class and how the script use the form and inputs element;

--- source/assets/scripts/index-script.js
+++ source/assets/scripts/index-script.js
@@ -1,14 +1,25 @@
 import * as globalScripts from "./global-scripts.js";
 import Triangle from "../../models/Triangle.js";
 
-const firstSideTriangleInput = document.querySelector("#firstSideTriangleInput");
-const secondSideTriangleInput = document.querySelector("#secondSideTriangleInput");
-const thirdSideTriangleInput = document.querySelector("#thirdSideTriangleInput");
-const triangleSidesForm = document.querySelector("main section form");
+const triangleSidesForm = document.querySelector(".triangleSidesForm");
+const allFormInputs = document.querySelectorAll(".triangleSidesForm input");
+const formResultParagraph = document.querySelector(".formResult")
 
 
 triangleSidesForm.addEventListener("submit", () => {
-    let triangle = new Triangle(firstSideTriangleInput.value, secondSideTriangleInput.value, thirdSideTriangleInput.value);
-    console.log(triangle.checkTriangle());
+    allFormInputs.forEach((input) => {
+        if (input.value === "") {
+            input.value = 0;
+        }
+    });
+    let triangle = new Triangle(allFormInputs[0].value, allFormInputs[1].value, allFormInputs[2].value);
+    writeTriangleResult(triangle.checkTriangle());
 })
 
+function writeTriangleResult(booleanResult) {
+    if (!booleanResult){
+        formResultParagraph.textContent = `This triangle don't exists.`;
+        return;
+    }
+    formResultParagraph.textContent = `This triangle exists.`;
+} 
--- source/models/Triangle.js
+++ source/models/Triangle.js
@@ -1,8 +1,8 @@
 export default class Triangle {
     constructor (firstSide, secondSide, thirdSide) {
-        this.firstSide = Number(firstSide);
-        this.secondSide = Number(secondSide);
-        this.thirdSide = Number(thirdSide);
+        this.firstSide = parseFloat(firstSide);
+        this.secondSide = parseFloat(secondSide);
+        this.thirdSide = parseFloat(thirdSide);
     }
     checkTriangle() {
         return ((this.firstSide + this.secondSide) > this.thirdSide && (this.secondSide + this.thirdSide) > this.firstSide && (this.firstSide + this.thirdSide) > this.secondSide);
